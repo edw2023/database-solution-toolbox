fe616821d73c46c66ea92004c836af625a2b5667
Lee, Powei <leepw1221@gmail.com>
2023-08-14 17:07:46 +0800

    optimize test data quality

--- src/main/java/com/pw/db/solution/toolbox/util/TestDataGenerator.java
+++ src/main/java/com/pw/db/solution/toolbox/util/TestDataGenerator.java
@@ -1,20 +1,11 @@
 package com.pw.db.solution.toolbox.util;
 
+import com.pw.db.solution.toolbox.util.testData.TestData;
+
 import java.util.Random;
 
 public class TestDataGenerator {
 
-    private static final String[] NAMES = {"Alice", "Bob", "Charlie", "David", "Eve"};
-    private static final String[] EMAIL_DOMAINS = {"example.com", "test.com", "sample.net"};
-    private static final String[] CITIES = {"New York", "Los Angeles", "Chicago", "Houston", "Miami"};
-    private static final String[] COUNTRIES = {"USA", "Canada", "UK", "Australia", "Germany"};
-    private static final String[] OCCUPATIONS = {"Engineer", "Teacher", "Doctor", "Designer", "Manager"};
-    private static final String[] INDUSTRIES = {"IT", "Education", "Healthcare", "Art", "Finance"};
-    private static final String[] MARITAL_STATUSES = {"Single", "Married", "Divorced"};
-    private static final String[] EDUCATION_LEVELS = {"High School", "Bachelor's Degree", "Master's Degree", "PhD"};
-    private static final String[] INTERESTS = {"Reading", "Traveling", "Sports", "Cooking", "Music"};
-    private static final String[] PRODUCT_CATEGORIES = {"Electronics", "Clothing", "Books", "Home Appliances", "Toys"};
-    private static final String[] CARD_TYPES = {"Visa", "MasterCard", "American Express"};
 
     private static final Random random = new Random();
 
@@ -24,53 +15,46 @@ public class TestDataGenerator {
     }
 
     public static String generateRandomName() {
-        return generateRandomItem(NAMES);
+        return generateRandomItem(TestData.NAMES);
     }
 
     public static String generateRandomEmail() {
-        int nameIndex = random.nextInt(NAMES.length);
-        int domainIndex = random.nextInt(EMAIL_DOMAINS.length);
-        return NAMES[nameIndex].toLowerCase() + "@" + EMAIL_DOMAINS[domainIndex];
+        int nameIndex = random.nextInt(TestData.NAMES.length);
+        int domainIndex = random.nextInt(TestData.EMAIL_DOMAINS.length);
+        return TestData.NAMES[nameIndex].toLowerCase() + "@" + TestData.EMAIL_DOMAINS[domainIndex];
     }
 
     public static int generateRandomAge() {
         return random.nextInt(50) + 20; // Generates ages between 20 and 69
     }
 
-    public static String generateRandomCity() {
-        return generateRandomItem(CITIES);
+    public static String generateRandomCity(String country) {
+        return generateRandomItem(TestData.COUNTRY_CITIES_MAP.get(country));
     }
 
     public static String generateRandomCountry() {
-        return generateRandomItem(COUNTRIES);
+        return generateRandomItem(TestData.COUNTRIES);
     }
 
     public static String generateRandomMaritalStatus() {
-        return generateRandomItem(MARITAL_STATUSES);
+        return generateRandomItem(TestData.MARITAL_STATUSES);
     }
 
     public static String generateRandomEducationLevel() {
-        return generateRandomItem(EDUCATION_LEVELS);
+        return generateRandomItem(TestData.EDUCATION_LEVELS);
     }
 
     public static String generateRandomOccupation() {
-        return generateRandomItem(OCCUPATIONS);
+        return generateRandomItem(TestData.OCCUPATIONS);
     }
 
-    public static String generateRandomIndustry() {
-        return generateRandomItem(INDUSTRIES);
-    }
 
     public static String generateRandomInterest() {
-        return generateRandomItem(INTERESTS);
-    }
-
-    public static String generateRandomProductCategory() {
-        return generateRandomItem(PRODUCT_CATEGORIES);
+        return generateRandomItem(TestData.INTERESTS);
     }
 
     public static String generateRandomCardType() {
-        return generateRandomItem(CARD_TYPES);
+        return generateRandomItem(TestData.CARD_TYPES);
     }
 
     // ... Add more methods for other fields
@@ -80,27 +64,23 @@ public class TestDataGenerator {
             String name = generateRandomName();
             String email = generateRandomEmail();
             int age = generateRandomAge();
-            String city = generateRandomCity();
             String country = generateRandomCountry();
+            String city = generateRandomCity(country);
             String maritalStatus = generateRandomMaritalStatus();
             String educationLevel = generateRandomEducationLevel();
             String occupation = generateRandomOccupation();
-            String industry = generateRandomIndustry();
             String interest = generateRandomInterest();
-            String productCategory = generateRandomProductCategory();
             String cardType = generateRandomCardType();
 
             System.out.println("Name: " + name);
             System.out.println("Email: " + email);
             System.out.println("Age: " + age);
-            System.out.println("City: " + city);
             System.out.println("Country: " + country);
+            System.out.println("City: " + city);
             System.out.println("Marital Status: " + maritalStatus);
             System.out.println("Education Level: " + educationLevel);
             System.out.println("Occupation: " + occupation);
-            System.out.println("Industry: " + industry);
             System.out.println("Interest: " + interest);
-            System.out.println("Product Category: " + productCategory);
             System.out.println("Card Type: " + cardType);
             System.out.println("------------------------");
         }
---
+++ src/main/java/com/pw/db/solution/toolbox/util/testData/TestData.java
@@ -0,0 +1,149 @@
+package com.pw.db.solution.toolbox.util.testData;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TestData {
+
+    public static final String[] NAMES = {"Alice", "Bob", "Charlie", "David", "Eve", "Frank", "Grace", "Henry", "Isabel", "Jack",
+            "Kate", "Leo", "Mia", "Nathan", "Olivia", "Paul", "Quinn", "Ryan", "Sophia", "Tom",
+            "Uma", "Vincent", "Willow", "Xavier", "Yara", "Zane", "Amelia", "Benjamin", "Chloe", "Daniel",
+            "Emma", "Finn", "Grace", "Henry", "Isabella", "Jacob", "Katherine", "Liam", "Mia", "Noah",
+            "Olivia", "Patrick", "Quinn", "Riley", "Sophia", "Thomas", "Uma", "Victor", "Willow", "Xander",
+            "Yara", "Zachary", "Ava", "Brian", "Charlotte", "David", "Ella", "Frederick", "Georgia", "Hannah",
+            "Isaac", "Jessica", "Kevin", "Lily", "Matthew", "Nora", "Oscar", "Penelope", "Quincy", "Rachel",
+            "Samuel", "Taylor", "Ulysses", "Victoria", "William", "Ximena", "Yasmine", "Zoe", "Andrew", "Bella",
+            "Christopher", "Daniel", "Eleanor", "Fiona", "George", "Hazel", "Isaiah", "Julia", "Kai", "Lucas",
+            "Michael", "Nathan", "Oliver", "Piper", "Quinn", "Riley", "Sophia", "Tristan", "Uma", "Vincent",
+            "William", "Xander", "Yara", "Zane","Oliver", "Ella", "Lucas", "Ava", "Liam", "Charlotte", "Mason", "Amelia", "Ethan", "Harper",
+            "Aiden", "Sophia", "Logan", "Isabella", "Jackson", "Mia", "Noah", "Olivia", "Caden", "Emily",
+            "Grayson", "Abigail", "Levi", "Avery", "Michael", "Evelyn", "Alexander", "Sofia", "Sebastian", "Scarlett",
+            "James", "Grace", "Benjamin", "Chloe", "Carter", "Zoey", "Joseph", "Lily", "Henry", "Madison",
+            "Owen", "Aubrey", "Wyatt", "Layla", "David", "Lillian", "Daniel", "Nora", "Matthew", "Hannah",
+            "Jayden", "Camila", "William", "Addison", "Muhammad", "Elizabeth", "Emily", "Sofia", "Ava", "Isabella",
+            "Olivia", "Mia", "Madison", "Liam", "Mason", "Ethan", "Noah", "Jacob", "Aiden", "Logan",
+            "Lucas", "Jackson", "Jack", "Caden", "Grayson", "Michael", "Benjamin", "Sebastian", "Alexander", "Daniel",
+            "James", "William", "Muhammad", "Emily", "Emma", "Olivia", "Ava", "Isabella", "Sophia", "Mia",
+            "Charlotte", "Amelia", "Harper", "Evelyn", "Abigail", "Emily", "Sophia", "Ava", "Isabella", "Olivia",
+            "Charlotte", "Oliver", "Ava", "Liam", "Isabella", "Mia", "Sophia", "Ethan", "Amelia", "Jackson",
+            "Harper", "Liam", "Sophia", "Ella", "Jack", "Olivia", "Daniel", "Chloe", "James", "Emily",
+            "Lucas", "Zoey", "William", "Abigail", "Logan", "Avery", "Matthew", "Evelyn", "Alexander", "Grace",
+            "Muhammad", "Addison", "Daniel", "Elizabeth", "Michael", "Nora", "William", "Camila", "Joseph", "Scarlett",
+            "Jayden", "Lily", "Muhammad", "Hannah", "Benjamin", "Lillian", "Jacob", "Nora", "Muhammad", "Evelyn",
+            "Emily", "Isabella", "Ava", "Sophia", "Olivia", "Mia", "Harper", "Charlotte", "Amelia", "Ella",
+            "Avery", "Scarlett", "Grace", "Addison", "Zoey", "Lillian", "Elizabeth", "Nora", "Evelyn", "Lily",
+            "Elizabeth", "Hannah", "Sofia", "Avery", "Grace", "Mia", "Harper", "Lily", "Isabella", "Amelia",
+            "Olivia", "Chloe", "Charlotte", "Ava", "Emily", "Ella", "Sophia", "Grace", "Evelyn", "Liam",
+            "Oliver", "Noah", "William", "Lucas", "Mason", "Logan", "Jackson", "Jack", "Muhammad", "Aiden"
+    };
+
+    public static final String[] OCCUPATIONS = {
+            "Engineer", "Teacher", "Doctor", "Designer", "Manager",
+            "Accountant", "Architect", "Writer", "Chef", "Nurse",
+            "Musician", "Artist", "Athlete", "Pilot", "Lawyer",
+            "Journalist", "Photographer", "Scientist", "Psychologist", "Mechanic",
+            "Electrician", "Plumber", "Carpenter", "Dentist", "Veterinarian",
+            "Librarian", "Police Officer", "Firefighter", "Paramedic", "Barista",
+            "Hairstylist", "Fashion Designer", "Actor", "Director", "Producer",
+            "Software Developer", "Entrepreneur", "Salesperson", "Marketing Manager", "HR Specialist",
+            "Financial Analyst", "Consultant", "Researcher", "Banker", "Economist",
+            "Translator", "Interpreter", "Singer", "Dancer", "Pharmacist"
+    };
+
+    public static final String[] COUNTRIES = {
+            "CHINA","USA","JAPAN","UK","FRANCE","GERMANY",
+            "SPAIN","ITALY","NETHERLANDS","AUSTRIA","PORTUGAL","CZECH_REPUBLIC","SWEDEN"
+    };
+
+    private static  String[] CHINA_CITIES = {
+            "Beijing", "Shanghai", "Guangzhou", "Shenzhen", "Chengdu",
+            "Hangzhou", "Nanjing", "Xi'an", "Wuhan", "Chongqing",
+    };
+
+
+    private static final String[] USA_CITIES = {
+            "New York", "Los Angeles", "Chicago", "Houston", "Miami",
+            "San Francisco", "Seattle", "Boston", "Atlanta", "Philadelphia",
+    };
+
+    private static final String[] JAPAN_CITIES = {
+            "Tokyo", "Osaka", "Kyoto", "Yokohama", "Nagoya",
+            "Sapporo", "Fukuoka", "Kobe", "Hiroshima", "Nara",
+    };
+
+    private static final String[] UK_CITIES = {
+            "London", "Manchester", "Birmingham", "Edinburgh", "Glasgow",
+            "Liverpool", "Bristol", "Leeds", "Newcastle", "Cardiff",
+    };
+
+    private static final String[] FRANCE_CITIES = {
+            "Paris", "Marseille", "Lyon", "Toulouse", "Nice",
+            "Nantes", "Strasbourg", "Montpellier", "Bordeaux", "Lille",
+    };
+
+    private static final String[] GERMANY_CITIES = {
+            "Berlin", "Munich", "Hamburg", "Cologne", "Frankfurt",
+            "Stuttgart", "Düsseldorf", "Leipzig", "Dresden", "Hannover",
+    };
+
+    private static final String[] SPAIN_CITIES = {
+            "Madrid", "Barcelona", "Valencia", "Seville", "Bilbao",
+            "Zaragoza", "Malaga", "Palma", "Murcia", "Alicante",
+    };
+
+    private static final String[] ITALY_CITIES = {
+            "Rome", "Milan", "Naples", "Turin", "Florence",
+            "Venice", "Bologna", "Genoa", "Palermo", "Verona",
+    };
+
+    private static final String[] NETHERLANDS_CITIES = {
+            "Amsterdam", "Rotterdam", "Utrecht", "The Hague", "Eindhoven",
+            "Groningen", "Nijmegen", "Tilburg", "Breda", "Maastricht",
+    };
+
+    private static final String[] AUSTRIA_CITIES = {
+            "Vienna", "Graz", "Linz", "Salzburg", "Innsbruck",
+            "Klagenfurt", "Villach", "Wels", "Sankt Pölten", "Dornbirn",
+    };
+
+    private static final String[] PORTUGAL_CITIES = {
+            "Lisbon", "Porto", "Vila Nova de Gaia", "Amadora", "Braga",
+            "Coimbra", "Funchal", "Setúbal", "Aveiro", "Evora",
+    };
+
+    private static final String[] CZECH_REPUBLIC_CITIES = {
+            "Prague", "Brno", "Ostrava", "Pilsen", "Liberec",
+            "Olomouc", "Usti nad Labem", "Hradec Kralove", "Ceske Budejovice", "Pardubice",
+    };
+
+    private static final String[] SWEDEN_CITIES = {
+            "Stockholm", "Gothenburg", "Malmö", "Uppsala", "Västerås",
+            "Örebro", "Linköping", "Helsingborg", "Jönköping", "Norrköping",
+    };
+
+    public static final Map<String, String[]> COUNTRY_CITIES_MAP = new HashMap<>();
+
+    static {
+        COUNTRY_CITIES_MAP.put("CHINA", CHINA_CITIES);
+        COUNTRY_CITIES_MAP.put("USA", USA_CITIES);
+        COUNTRY_CITIES_MAP.put("JAPAN", JAPAN_CITIES);
+        COUNTRY_CITIES_MAP.put("UK", UK_CITIES);
+        COUNTRY_CITIES_MAP.put("FRANCE", FRANCE_CITIES);
+        COUNTRY_CITIES_MAP.put("GERMANY", GERMANY_CITIES);
+        COUNTRY_CITIES_MAP.put("SPAIN", SPAIN_CITIES);
+        COUNTRY_CITIES_MAP.put("ITALY", ITALY_CITIES);
+        COUNTRY_CITIES_MAP.put("NETHERLANDS", NETHERLANDS_CITIES);
+        COUNTRY_CITIES_MAP.put("AUSTRIA", AUSTRIA_CITIES);
+        COUNTRY_CITIES_MAP.put("PORTUGAL", PORTUGAL_CITIES);
+        COUNTRY_CITIES_MAP.put("CZECH_REPUBLIC", CZECH_REPUBLIC_CITIES);
+        COUNTRY_CITIES_MAP.put("SWEDEN", SWEDEN_CITIES);
+    }
+
+    public static final String[] EMAIL_DOMAINS = {"example.com", "test.com", "sample.net"};
+//    public static final String[] CITIES = {"New York", "Los Angeles", "Chicago", "Houston", "Miami"};
+    public static final String[] MARITAL_STATUSES = {"Single", "Married", "Divorced"};
+    public static final String[] EDUCATION_LEVELS = {"High School", "Bachelor's Degree", "Master's Degree", "PhD"};
+    public static final String[] INTERESTS = {"Reading", "Traveling", "Sports", "Cooking", "Music"};
+    public static final String[] CARD_TYPES = {"Visa", "MasterCard", "American Express"};
+
+}
